77153f93b6a7613b8aa5e061719225bf5d755d77
HRK31 <hrk10321@gmail.com>
2020-08-13 13:26:46 +0530

    Modified recipe-detail and recipe-list components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,7 @@ import { Recipe } from "../recipe.model";
 export class RecipeListComponent implements OnInit {
 
 recipes: Recipe[] = [
+  new Recipe("A Test Recipe",'This is simple a test','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/epic-summer-salad.jpg'),
   new Recipe("A Test Recipe",'This is simple a test','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/epic-summer-salad.jpg')
 ];
 
