0b1e56aca4c471e4341d61aacc7d8f17a0815a6b
HRK31 <hrk10321@gmail.com>
2020-08-14 13:51:42 +0530

    Added the functionality of onNavigate()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shoppinglistrecipebook';
+
+  loadedvalue  = 'recipe';
+
+  onNavigate(value: string){
+    this.loadedvalue = value;
+  }
 }
